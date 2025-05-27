bf25f0f9a781728cf4c7999d95e3912bd9b3fcbc
F Cesar Farias <fcesarf80@gmail.com>
2025-05-27 21:40:52 +0100

    desconto Cpp

---
+++ ex03_adding strings.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(int arg, char** argv) {
+
+	/*int myInt = 10;
+	float = myFloat = 0.0f;
+	double myDouble = 0.0;
+
+	bool myBool = true;
+	char myChar = 't';
+	char myChar = 'c'
+	char myArray[11] = "0123456789";
+
+	size_t mySizeT = 0;*/
+
+std::string myString = "Teste";
+	myString = "Zumba ";
+	myString += "Buldogue";
+	std::cout << myString<<"\n";
+	system("pause");
+
+	return 0;
+
+}
